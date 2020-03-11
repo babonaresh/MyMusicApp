bf7026781f8fc4a11b6f54f26435f63bd1efe773
Naresh B <nareshyadav.us@gmail.com>
2020-03-11 12:54:25 -0500

    adding Gig form

---
+++ MyMusicApp/Controllers/GigsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMusicApp.Models;
+using MyMusicApp.ViewModels;
+
+namespace MyMusicApp.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres= _context.Genre.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
